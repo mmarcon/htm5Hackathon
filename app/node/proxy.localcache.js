ee2f2115347e8a1089bb71f54421e427d8c6a398
Massimiliano Marcon <emaildelmax@gmail.com>
2012-05-06 09:35:21 +0200

    Minor fixes here and there. As a reference added node proxy that stores mp3s before serving them.

---
+++ app/node/proxy.localcache.js
@@ -0,0 +1,32 @@
+var http = require('http'),
+	  url = require('url'),
+    path = require('path'),
+    express = require('express'),
+    app = express.createServer(),
+    fs = require('fs');
+
+
+app.get('/proxy', function(request, response){
+  var params = url.parse(request.url, true).query;
+  var requestURL = url.parse(params.url); 
+  
+  var proxy = http.createClient(80, requestURL.host);
+  var proxy_request = proxy.request(request.method, requestURL.href, request.headers);
+  proxy_request.on('response', function (proxy_response) {
+      var fileName = Date.now() + '__audio.mp3';
+      var fStream =  fs.createWriteStream(path.normalize(__dirname + '/../media/' + fileName));
+      proxy_response.pipe(fStream);
+      fStream.on('close', function(){
+        console.log(fileName + ' written.');
+        response.writeHead(302, {
+          'Location': '/media/' + fileName
+        });
+        response.end();
+      });
+  });
+  request.pipe(proxy_request);
+});
+
+app.use(express.static(path.normalize(__dirname + '/..')));
+
+app.listen(8000);
