b947de163c4d6c443cb176a0efe5dbc8774b0b4e
Massimiliano Marcon <emaildelmax@gmail.com>
2012-05-08 20:03:01 +0200

    Avoid being an anonymous proxy.

--- node/proxy.js
+++ node/proxy.js
@@ -8,17 +8,26 @@ var http = require('http'),
 app.get('/proxy', function (request, response) {
     var params = url.parse(request.url, true).query;
     var requestURL = url.parse(params.url);
-
-    var proxy = http.createClient(80, requestURL.host)
-    var proxy_request = proxy.request(request.method, requestURL.href, request.headers);
-    proxy_request.on('response', function (proxy_response) {
-        proxy_response.pipe(response);
-        var h = proxy_response.headers;
-        response.writeHead(proxy_response.statusCode, h);
-    });
-    request.pipe(proxy_request);
+    //Only proxy requests for deezer!
+    if (requestURL.host.indexOf('deezer.com') > -1) {
+        var proxy = http.createClient(80, requestURL.host)
+        var proxy_request = proxy.request(request.method, requestURL.href, request.headers);
+        proxy_request.on('response', function (proxy_response) {
+            proxy_response.pipe(response);
+            var h = proxy_response.headers;
+            response.writeHead(proxy_response.statusCode, h);
+        });
+        request.pipe(proxy_request);
+    }
+    else {
+        //We don't wanna be an anonymous proxy, do we?
+        //It is not deezer, so send back an error
+        //400: BAD BAD BAD Request!
+        response.writeHead(400);
+        response.end();
+    }
 });
 
-app.use(express.static(path.normalize(__dirname + '/../app/')));
+app.use(express.static(path.normalize(__dirname + '/app/')));
 
 app.listen(8000);
